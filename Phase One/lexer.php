4beb3de0d4cf360deefc026cd25b97a984ed69c8
kokhouser <hzk11a@acu.edu>
2014-09-23 22:26:32 -0500

    Started work on lexer

---
+++ Phase One/lexer.php
@@ -0,0 +1,28 @@
+<?php
+
+	/* Plan of attack:
+		0. Make sure it's a .ram file
+		1. Strip comments.
+		2. Create input stream, make sure endlines are intact.
+
+		ISSUES:
+		- You HAVE to have a space before the "#" in a comment that's on the same line as a line of code.
+	*/
+
+	function multiexplode ($delimiters, $string){
+		$ready = str_replace($delimiters, $delimiters, $string);
+		$launch = explode($delimiters[0], $ready);
+		return $launch;
+	}
+
+	$filename = $argv[1];
+	$input = file_get_contents($filename);
+	$lines = explode("\n", $input);
+	$no_comments = array();
+	$count = 0;
+	foreach($lines as $line){
+		$pretoken = explode(" ",$line);
+		echo trim($pretoken[0]);
+	}
+
+?>
