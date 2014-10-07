819071cfb99a0fc351f0aa5e6c0efd5a274069fc
kokhouser <hzk11a@acu.edu>
2014-10-07 10:36:50 -0500

    Tested parser

--- Phase One/parser.php
+++ Phase One/parser.php
@@ -2,7 +2,8 @@
 
   $filename = "token.txt";
   $input = file_get_contents($filename);
-  $tokens = explode("\n", input);
-  print_r($tokens);
+  $tokens = explode("\n", $input);
+  print_r($tokens); //Puts tokens into array, one token per array element.
+  //Note that there are n+1 array slots because of the extra endline.
 
 ?>
