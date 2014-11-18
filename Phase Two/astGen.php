2c00a931feea0ee65d151fc5d3b9c0e9d2910a88
BINBINWU <bxw10a@acu.edu>
2014-11-18 15:02:10 -0600

    It's my time to be awesome!
    I created grammar3, and it has the new grammar for expression

--- Phase Two/astGen.php
+++ Phase Two/astGen.php
@@ -28,6 +28,7 @@
 	class ast{
 		private $nodes; //array of nodes, root is always at 0
 		private $index; //current end for adding to
+<<<<<<< Updated upstream
 		private $tokenArray;
 
 		public function get_tokenArray(){
@@ -37,6 +38,10 @@
 		public function set_tokenArray($input){
 			$this->tokenArray = $input;
 		}
+=======
+        
+        
+>>>>>>> Stashed changes
 		
 		public function __construct(){
 			$this->index=0;
